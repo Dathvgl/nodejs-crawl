0cc3f0351e7eb2723e17817c1b9b929339122018
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-01-26 11:19:08 +0700

    revoke handle?

--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -15,10 +15,13 @@ export async function authFirebaseHandler(
     res.status(404).json({ message: "Không xác thực" });
   } else {
     let pass = false;
+    let revoke = false;
+
     const length = auths.length;
 
     for (let index = 0; index < length; index++) {
       const auth = auths[index];
+
       try {
         const decodedToken = await auth.verifySessionCookie(session, true);
 
@@ -27,11 +30,28 @@ export async function authFirebaseHandler(
         break;
       } catch (e) {
         const error = e as FirebaseError;
+
+        console.error(`Firebase ${index}`);
+        console.error(error.code);
         console.error(error.message);
+
+        if (error.code == "auth/session-cookie-revoked") {
+          revoke = true;
+          res.clearCookie("crawl-auth");
+          const decodedClaims = await auth.verifySessionCookie(session);
+          await auth.revokeRefreshTokens(decodedClaims.sub);
+          break;
+        }
       }
     }
 
     if (pass) next();
-    else res.status(404).json({ message: "Không xác thực" });
+    else {
+      if (revoke) {
+        res.status(200).json({ message: "Xóa xác thực" });
+      } else {
+        res.status(404).json({ message: "Không xác thực" });
+      }
+    }
   }
 }
