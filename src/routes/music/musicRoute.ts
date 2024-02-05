e47a9619584c6600b039ee3348447a42a9546c44
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-02-05 23:47:50 +0700

    fix name

--- src/routes/music/musicRoute.ts
+++ src/routes/music/musicRoute.ts
@@ -3,6 +3,6 @@ import zingMP3Router from "./zingMP3/zingMP3Route";
 
 const musicRouter = Router();
 
-musicRouter.use("/zingmp3", zingMP3Router);
+musicRouter.use("/zingMP3", zingMP3Router);
 
 export default musicRouter;
